59f9442acb33029d6d880999289788d3f1b90e74
Janghyub Seo <seojanghyeob@gmail.com>
2020-11-15 00:15:33 +0900

    Add dokka generation and workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,27 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("org.jetbrains.dokka") version "1.4.10.2"
 }
 group = "dev.jhseo"
 version = "1.0.0"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
-tasks.withType<Test>() {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.dokkaGfm.configure {
+    outputDirectory.set(rootDir.resolve("docs"))
+    moduleName.set("${version}")
 }
 
 val kotestVersion = "4.3.1"
@@ -23,4 +30,6 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:${kotestVersion}") // for kotest framework
     testImplementation("io.kotest:kotest-assertions-core:${kotestVersion}") // for kotest core jvm assertions
     testImplementation("io.kotest:kotest-property:${kotestVersion}") // for kotest property test
+
+    dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.4.10.2")
 }
