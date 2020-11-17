84703e517e0197aa63b5cb3650980da34deebfa5
Janghyub Seo <seojanghyeob@gmail.com>
2020-11-17 14:00:16 +0900

    Implement String.hash()

--- src/main/kotlin/dev/jhseo/hasher/HasherString.kt
+++ src/main/kotlin/dev/jhseo/hasher/HasherString.kt
@@ -3,6 +3,5 @@ package dev.jhseo.hasher
 import java.nio.charset.Charset
 
 fun String.hash(method: HasherMethod, charset: Charset = Charsets.UTF_8) : String {
-
-    return ""
+    return Hasher.hash(this, method, charset)
 }
